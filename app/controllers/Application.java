87a728448d2d82f392a8966ae9d0754a3b324481
Mario Lujan <darkbloodlie@hotmail.es>
2015-10-27 22:54:40 -0700

    5 Captcha Acabado

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,7 +6,10 @@ import play.*;
 import play.mvc.*;
  
 import models.*;
+import play.cache.Cache;
 import play.data.validation.Required;
+import play.libs.Codec;
+import play.libs.Images;
  
 public class Application extends Controller {
     
@@ -15,6 +18,14 @@ static void addDefaults() {
     renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
     renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
 }
+
+public static void captcha(String id) {
+    Images.Captcha captcha = Images.captcha();
+    String code = captcha.getText("#E4EAFD");
+    Cache.set(id, code, "10mn");
+    renderBinary(captcha);
+}
+
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
         List<Post> olderPosts = Post.find(
@@ -25,17 +36,28 @@ static void addDefaults() {
     
     public static void show(Long id) {
     Post post = Post.findById(id);
-    render(post);
-    }
+    String randomID = Codec.UUID();
+    render(post, randomID);
+}
  
     
-    public static void postComment(Long postId, @Required String author, @Required String content) {
+    public static void postComment(
+        Long postId, 
+        @Required(message="Author is required") String author, 
+        @Required(message="A message is required") String content, 
+        @Required(message="Please type the code") String code, 
+        String randomID) 
+{
     Post post = Post.findById(postId);
+    validation.equals(
+        code, Cache.get(randomID)
+    ).message("Invalid code. Please type it again");
     if(validation.hasErrors()) {
-        render("Application/show.html", post);
+        render("Application/show.html", post, randomID);
     }
     post.addComment(author, content);
     flash.success("Thanks for posting %s", author);
+    Cache.delete(randomID);
     show(postId);
 }
     
